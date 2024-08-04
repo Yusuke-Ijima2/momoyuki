60ee7bc3c4daa72a35ec7fe70eb636489a10ef38
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2024-08-04 19:24:37 +0900

    <SessionProvider basePath="/api/auth">追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={inter.className}>
-        <Toaster />
-        {children}
+        <SessionProvider basePath="/api/auth">
+          <Toaster />
+          {children}
+        </SessionProvider>
       </body>
     </html>
   );
