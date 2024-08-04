71990818aeb3e394a7d5b46dd374873e23b709d8
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2024-08-04 19:15:56 +0900

    環境変数リネーム

--- src/app/_component/map-view.tsx
+++ src/app/_component/map-view.tsx
@@ -190,7 +190,7 @@ const MapView = ({ center }: Props) => {
     <>
       <SearchBox onPlaceChanged={handlePlaceChanged} />
       <Wrapper
-        apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}
+        apiKey={process.env.GOOGLE_MAPS_API_KEY!}
         render={render}
         libraries={["places"]}
       >
