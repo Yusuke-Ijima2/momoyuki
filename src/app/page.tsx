f1647d055263482ac95859b07ec29372b4f04503
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2024-08-06 23:17:50 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,16 @@ import ViewPost from "./_component/view-post";
 import MapView from "./_component/map-view";
 
 const Home = async () => {
-  const session = await getServerSession();
+  // const session = await getServerSession();
 
   return (
     <div className="space-y-2 p-1">
       <p className="text-xs">v1.1 ももかとゆきちゃんのために作ったアプリ</p>
       <MapView />
       <ViewPost />
-      <div className="p-4">
+      {/* <div className="p-4">
         {session && `${session.user.name}でログイン中`} <Auth />
-      </div>
+      </div> */}
     </div>
   );
 };
